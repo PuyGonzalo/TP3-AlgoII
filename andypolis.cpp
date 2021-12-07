7911e8ed20eafcf4cad50c956264f1081422b0b4
Pintos Gaston <gmpintos@fi.uba.ar>
2021-12-07 18:11:19 -0300

    Se arregle atacar para un edificio, falla para dos

--- andypolis.cpp
+++ andypolis.cpp
@@ -404,6 +404,8 @@ Estado_t Andypolis::atacar_edificio_de_coord(int coord_x, int coord_y, Jugador_t
             posicion_edificio = jugador_dos.buscar_edificio_por_nombre(nombre_edificio);
             orden_edificio = jugador_dos.buscar_posicion_coordenadas( posicion_edificio, coord_x, coord_y);
 
+            cout<< "SELECCIONO" << nombre_edificio << posicion_edificio << orden_edificio;
+
             if( jugador_dos.obtener_vida_edificio( posicion_edificio, orden_edificio) == 100 ){
                     jugador_dos.restar_vida_edificio( posicion_edificio, orden_edificio);
             }
@@ -426,8 +428,10 @@ Estado_t Andypolis::atacar_edificio_de_coord(int coord_x, int coord_y, Jugador_t
         }
         else {
             nombre_edificio = mapa.obtener_nombre_objeto_de_casillero_ocupado(coord_x, coord_y);
-            posicion_edificio = jugador_dos.buscar_edificio_por_nombre(nombre_edificio);
-            orden_edificio = jugador_dos.buscar_posicion_coordenadas( posicion_edificio, coord_x, coord_y);
+            posicion_edificio = jugador_uno.buscar_edificio_por_nombre(nombre_edificio);
+            orden_edificio = jugador_uno.buscar_posicion_coordenadas( posicion_edificio, coord_x, coord_y);
+
+            cout<< "SELECCIONO" << nombre_edificio << posicion_edificio << orden_edificio;
 
             if( jugador_uno.obtener_vida_edificio(posicion_edificio, orden_edificio) == 100 ){
                     jugador_uno.restar_vida_edificio(posicion_edificio, orden_edificio);
